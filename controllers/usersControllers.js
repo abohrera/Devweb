c9bad8dc7ae591e665804e9ad3745c47a24fad2c
abohrera <113165926+abohrera@users.noreply.github.com>
2024-11-21 16:09:39 +0100

    Update usersControllers.js

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -44,3 +44,65 @@ exports.createNewUser = (req, res) => {
 		}
 	)
 }
+
+// POST mise à jour d'un utilisateur en fonction de son ID
+exports.updateUser = (req, res) => {
+	const { firstName, lastName } = req.body
+
+	// Récupérer l'id des paramètres
+	const userId = req.params.id
+
+	// Vérifier les champs envoyés
+	let updateFields = []
+	let queryParams = []
+
+	if (firstName) {
+		updateFields.push("firstName = ?")
+		queryParams.push(firstName)
+	}
+
+	if (lastName) {
+		updateFields.push("lastName = ?")
+		queryParams.push(lastName)
+	}
+
+	if (updateFields.length > 0) {
+		// Ajouter userId aux paramètres de la requête
+		queryParams.push(userId)
+
+		// Construire la requête dynamiquement
+		const query = `UPDATE users SET ${updateFields.join(", ")} WHERE id = ?`
+
+		db.run(query, queryParams, function (err) {
+			if (err) {
+				res.status(500).json({ error: err.message })
+			} else if (this.changes === 0) {
+				res.status(404).json({ message: "Utilisateur non trouvé" })
+			} else {
+				res.json({ msg: "Utilisateur mis à jour", userId, firstName, lastName })
+			}
+		})
+	} else {
+		res.status(400).json({ message: "Aucun champ à mettre à jour" })
+	}
+}
+
+
+
+// delete 
+exports.deleteUser = (req, res) => {
+	// get the id from the params
+	const { id } = req.params
+	// run the query
+	db.run("DELETE FROM users WHERE id = ?", [id], function (err) {
+		if (err) {
+			res.status(500).json({ error: err.message })
+		} else if (this.changes === 0) {
+			// if nothing found
+			res.status(404).json({ message: "User not found" })
+		} else {
+			// is successful
+			res.status(200).json({ message: "User deleted !" })
+		}
+	})
+}
